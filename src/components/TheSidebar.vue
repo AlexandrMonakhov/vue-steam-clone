6b936ab0d14ca6a32c506a0317125cc036d10a76
AlexandrMonakhov <a.monahov@mail.ru>
2022-11-11 16:56:05 +0300

    SC-2 / Добавлен слайдер + контроллер к слайдеру и создана оболочка для сайдбара

---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="sidebar">
+		<div class="sidebar__action">
+
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Sidebar',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/vars.scss';
+
+.sidebar {
+	width: 100px;
+	background-color: $color_primary_alt;
+	height: calc(100vh - 80px);
+	mix-blend-mode: normal;
+	box-shadow: 4px 0px 6px rgba($color_black, .12);
+}
+</style>
--- src/controllers/slider-controller.js
+++ src/controllers/slider-controller.js
@@ -1,5 +1,40 @@
 class SliderController {
 	currentSlideIndex = 0;
+	images = [];
+
+	isActiveSlide(index) {
+		return this.currentSlideIndex === index;
+	}
+
+	setTranslate() {
+		return {
+			transform: `translateX(-${856 * this.currentSlideIndex}px)`
+		};
+	}
+
+	setImages(images) {
+		this.images = images;
+	}
+
+	prev() {
+		this.currentSlideIndex--;
+
+		if (this.currentSlideIndex < 0) {
+			this.currentSlideIndex = this.images.length - 1;
+		}
+	}
+
+	next() {
+		this.currentSlideIndex++;
+
+		if (this.currentSlideIndex >= this.images.length) {
+			this.currentSlideIndex = 0;
+		}
+	}
+
+	onDotClick(index) {
+		this.currentSlideIndex = index;
+	}
 }
 
 export {
